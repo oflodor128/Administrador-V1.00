5a0dd2777257c622a7770a83c0056344f9d802fe
oflodor128 <oflodor128@gmail.com>
2022-10-30 22:41:38 -0700

    first commit

---
+++ src/scenes/geography/index.jsx
@@ -0,0 +1,21 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import GeographyChart from "../../components/Geography";
+import { tokens } from "../../theme";
+import { useTheme } from "@mui/material";
+
+const Geography = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+      <Header title="Line Chart" subtitle="Lines" />
+
+      <Box height="75vh" border={`1px solid ${colors.grey[100]}`} borderRadius="4px">
+        <GeographyChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Geography;
